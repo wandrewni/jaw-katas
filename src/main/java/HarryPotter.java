0c15ec323e334d00c3dc0000aef75486e1376e6c
Andrew Nicholson <Andrew.Nicholson@ithaka.org>
2018-08-15 20:09:11 -0400

    working more on harry potter

--- src/main/java/HarryPotter.java
+++ src/main/java/HarryPotter.java
@@ -1,20 +1,58 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class HarryPotter {
     static final int BOOK_PRICE = 800;
-    static final double[] DISCOUNT_RATE = new double[] {0, 0, .05, .10, .20, .25};
+    static final double[] DISCOUNT_RATE = new double[]{0, 0, .05, .10, .20, .25};
 
     public int getTotalPrice(int[] books) {
-        int bookQuantity = books.length;
-        final IntStream distinctedStream = Arrays.stream(books).distinct();
-        long numberOfDifferentBooks = distinctedStream.count();
-        final int subTotal = BOOK_PRICE * bookQuantity;
-        int totalPrice = subTotal - calcSetDiscount((int) numberOfDifferentBooks);
+        Collection<Set<Integer>> bookSets = sortIntoSets(books);
+
+        int totalPrice = 0;
+        for (Set<Integer> bookSet : bookSets) {
+            totalPrice += calcSetPrice(bookSet.size());
+        }
         return totalPrice;
     }
 
-    private int calcSetDiscount(int booksInSet){
-        return booksInSet * (int)(BOOK_PRICE * DISCOUNT_RATE[booksInSet]);
+    private Collection<Set<Integer>> sortIntoSets(int[] books) {
+        Collection<Set<Integer>> bookSets = new ArrayList<>();
+
+        for (int book : books) {
+            boolean bookAddedToSet = addBookToBookSets(bookSets, book);
+            if (!bookAddedToSet) {
+                Set<Integer> newBookSet = new HashSet<>();
+                newBookSet.add(book);
+                bookSets.add(newBookSet);
+            }
+        }
+        return bookSets;
+    }
+
+    private boolean addBookToBookSets(Collection<Set<Integer>> bookSets, int book) {
+        for (Set<Integer> bookSet : bookSets) {
+            if (bookSet.size() == 3 && !bookSet.contains(book)){
+                bookSet.add(book);
+                return true;
+            }
+        }
+        for (Set<Integer> bookSet : bookSets) {
+            if (!bookSet.contains(book)) {
+                bookSet.add(book);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int calcSetPrice(int numBooks) {
+        int subTotal = BOOK_PRICE * numBooks;
+        return subTotal - calcSetDiscount(numBooks);
+    }
+
+    private int calcSetDiscount(int booksInSet) {
+        return booksInSet * (int) (BOOK_PRICE * DISCOUNT_RATE[booksInSet]);
     }
 }
--- src/test/java/HarryPotterTest.java
+++ src/test/java/HarryPotterTest.java
@@ -3,51 +3,94 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class HarryPotterTest {
-    private static final int BOOK_ONE = 1;
-    private static final int BOOK_TWO = 2;
-    private static final int BOOK_THREE = 3;
-    private static final int BOOK_FOUR = 4;
-    private static final int BOOK_FIVE = 5;
-    HarryPotter harryPotter = new HarryPotter();
+    private static final int BOOK_1 = 1;
+    private static final int BOOK_2 = 2;
+    private static final int BOOK_3 = 3;
+    private static final int BOOK_4 = 4;
+    private static final int BOOK_5 = 5;
+    private HarryPotter harryPotter = new HarryPotter();
 
     @Test
     public void zeroBooksIsZeroDollars() {
-        assertEquals(BOOK_ONE, harryPotter.getTotalPrice(new int[]{}));
+        verifyPrice(0, new int[]{});
     }
 
     @Test
     public void oneBookIsEightDollars() {
-        final int[] books = {BOOK_ONE};
-        assertEquals(800, harryPotter.getTotalPrice(books));
+        final int[] books = {BOOK_1};
+        final int expectedPrice = 800;
+        verifyPrice(expectedPrice, books);
     }
 
     @Test
     public void twoBooksHaveFivePercentDiscount() {
-        final int[] books = {BOOK_ONE, BOOK_TWO};
-        assertEquals(1520, harryPotter.getTotalPrice(books));
+        final int[] books = {BOOK_1, BOOK_2};
+        verifyPrice(1520, books);
     }
 
     @Test
     public void threeBooksHave10PercentDiscount() {
-        final int[] books = {BOOK_ONE, BOOK_TWO, BOOK_THREE};
-        assertEquals(2160, harryPotter.getTotalPrice(books));
+        final int[] books = {BOOK_1, BOOK_2, BOOK_3};
+        verifyPrice(2160, books);
     }
 
     @Test
     public void fourBooksHave20PercentDiscount() {
-        final int[] books = {BOOK_ONE, BOOK_TWO, BOOK_THREE, BOOK_FOUR};
-        assertEquals(2560, harryPotter.getTotalPrice(books));
+        final int[] books = {BOOK_1, BOOK_2, BOOK_3, BOOK_4};
+        verifyPrice(2560, books);
     }
 
     @Test
     public void fiveBooksHas25PercentDiscount() {
-        final int[] books = {BOOK_ONE, BOOK_TWO, BOOK_THREE, BOOK_FOUR, BOOK_FIVE};
-        assertEquals(3000, harryPotter.getTotalPrice(books));
+        final int[] books = {BOOK_1, BOOK_2, BOOK_3, BOOK_4, BOOK_5};
+        verifyPrice(3000, books);
     }
 
     @Test
     public void twoSameBooksGetsNoDiscount() {
-        final int[] books = {BOOK_ONE, BOOK_ONE};
-        assertEquals(1600, harryPotter.getTotalPrice(books));
+        final int[] books = {BOOK_1, BOOK_1};
+        verifyPrice(1600, books);
+    }
+
+    @Test
+    public void onlySetBooksGetSetDiscount() throws Exception {
+        final int[] books = {BOOK_1, BOOK_2, BOOK_2};
+        verifyPrice(2320, books);
+        final int[] threeAndDuplicate = {BOOK_1, BOOK_2, BOOK_3, BOOK_3};
+        verifyPrice(2960, threeAndDuplicate);
+        final int[] threeAndTwoDuplicates = {BOOK_1, BOOK_2, BOOK_2, BOOK_3, BOOK_3};
+        verifyPrice(3680, threeAndTwoDuplicates);
+    }
+
+    @Test
+    public void canGetSetDiscountsOnThreeSets() throws Exception {
+        final int[] threeSets = {
+                BOOK_1, BOOK_2, BOOK_3,
+                BOOK_1, BOOK_2, BOOK_3,
+                BOOK_1, BOOK_2, BOOK_3
+        };
+        verifyPrice(6480, threeSets);
+    }
+
+    @Test
+    public void choosesBestPriceWhenTwoPossibilities() throws Exception {
+        final int[] twoSetsOfFour_versus_setOfThreeAndSetOfFive = {
+                BOOK_1, BOOK_2, BOOK_3, BOOK_4,
+                BOOK_2, BOOK_3, BOOK_4, BOOK_5
+        };
+        verifyPrice(5120, twoSetsOfFour_versus_setOfThreeAndSetOfFive);
+    }
+
+    @Test
+    public void choosesBestPriceWhenTwoPossibilities_InTroublesomeOrder() throws Exception {
+        final int[] twoSetsOfFour_versus_setOfThreeAndSetOfFive = {
+                BOOK_1, BOOK_2, BOOK_3, BOOK_4,
+                BOOK_5, BOOK_2, BOOK_3, BOOK_4
+        };
+        verifyPrice(5120, twoSetsOfFour_versus_setOfThreeAndSetOfFive);
+    }
+
+    private void verifyPrice(int expectedPrice, int[] books) {
+        assertEquals(expectedPrice, harryPotter.getTotalPrice(books));
     }
 }
