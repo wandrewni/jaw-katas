217948a1f67b73837c73f6534bcbf1af6b481503
Jesse <jesse-github@istheguy.com>
2018-11-16 20:03:34 -0500

    completed ChristmasCoordinates program

--- advent-of-code-2016-day-1/src/main/java/advent_of_code/ChristmasCoordinates.java
+++ advent-of-code-2016-day-1/src/main/java/advent_of_code/ChristmasCoordinates.java
@@ -2,4 +2,60 @@ package advent_of_code;
 
 public class ChristmasCoordinates {
 
+	public static int calcShortestPath(String directions) {
+		int x = 0;
+		int y = 0;
+		Direction direction = Direction.N;
+		
+		if (directions.length() == 0) {
+			return 0;
+		}
+		
+		String[] steps = directions.replaceAll(",", "").split(" ");
+		for (String step: steps) {
+			direction = newDirection(direction, isLeft(step));
+			switch(direction) {
+			case E:
+				x += parseLength(step);
+				break;
+			case N:
+				y += parseLength(step);
+				break;
+			case S:
+				y -= parseLength(step);
+				break;
+			case W:
+				x -= parseLength(step);
+				break;
+			default:
+				break;
+			}
+		}
+		return (Math.abs(x)+Math.abs(y));
+	}
+
+	private static int parseLength(String step) {
+		return Integer.parseInt(step.substring(1));
+	}
+	
+	private static boolean isLeft(String step) {
+		return step.charAt(0) == 'L';
+	}
+	
+	private static Direction newDirection(Direction currentDirection, boolean turningLeft) {
+		switch (currentDirection) {
+			case N:
+				return turningLeft ? Direction.W : Direction.E;
+			case E:
+				return turningLeft ? Direction.N : Direction.S;
+			case S:
+				return turningLeft ? Direction.E : Direction.W;
+			case W:
+				return turningLeft ? Direction.S : Direction.N;
+			default:
+				return null;
+		}
+	}
+		
+	enum Direction{N, E, S, W};
 }
--- advent-of-code-2016-day-1/src/test/java/advent_of_code/ChristmasCoordinatesTest.java
+++ advent-of-code-2016-day-1/src/test/java/advent_of_code/ChristmasCoordinatesTest.java
@@ -1,5 +1,48 @@
 package advent_of_code;
 
-public class ChristmasCoordinatesTest {
+import static advent_of_code.ChristmasCoordinates.calcShortestPath;
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+public class ChristmasCoordinatesTest {
+	
+	@Test
+	public void calcShortestPathForNoInput(){
+		assertEquals(0, calcShortestPath(""));
+	}
+
+	@Test
+	public void shortestPathForOneStepIsOne() {
+		assertEquals(1, calcShortestPath("L1"));
+		assertEquals(1, calcShortestPath("R1"));
+	}
+	
+	@Test
+	public void shortestPathForSinglePathIsLengthOfPath() {
+		assertEquals(3, calcShortestPath("L3"));
+		assertEquals(3, calcShortestPath("R3"));
+	}
+	
+	@Test
+	public void shortestForTwoOneStepsIsTwo() {
+		assertEquals(2, calcShortestPath("L1, L1"));
+	}
+	
+	@Test
+	public void severalTwoStepCases() throws Exception {
+		assertEquals(10, calcShortestPath("L4, L6"));
+		assertEquals(103, calcShortestPath("R41, L62"));
+	}
+	
+	@Test
+	public void threeSingleLengthStepsInOneDirectionIsOne() {
+		assertEquals(1, calcShortestPath("L1, L1, L1"));
+		assertEquals(1, calcShortestPath("R1, R1, R1"));
+	}
+	
+	@Test
+	public void threeSingleLengthStepsVaryingWaysIsThree() {
+		assertEquals(3, calcShortestPath("L1, L1, R1"));
+	}
 }
