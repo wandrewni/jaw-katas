632aaf25ff2ccc5dd7ed2cb831d0c6fa305b41dd
Andrew Nicholson <Andrew.Nicholson@ithaka.org>
2018-11-16 20:07:27 -0500

    add puzzle input for day one

--- advent-of-code-2016-day-1/src/test/java/advent_of_code/ChristmasCoordinatesTest.java
+++ advent-of-code-2016-day-1/src/test/java/advent_of_code/ChristmasCoordinatesTest.java
@@ -45,4 +45,10 @@ public class ChristmasCoordinatesTest {
 	public void threeSingleLengthStepsVaryingWaysIsThree() {
 		assertEquals(3, calcShortestPath("L1, L1, R1"));
 	}
+
+	@Test
+	public void puzzleInput() throws Exception {
+		String puzzleInput = "L3, R1, L4, L1, L2, R4, L3, L3, R2, R3, L5, R1, R3, L4, L1, L2, R2, R1, L4, L4, R2, L5, R3, R2, R1, L1, L2, R2, R2, L1, L1, R2, R1, L3, L5, R4, L3, R3, R3, L5, L190, L4, R4, R51, L4, R5, R5, R2, L1, L3, R1, R4, L3, R1, R3, L5, L4, R2, R5, R2, L1, L5, L1, L1, R78, L3, R2, L3, R5, L2, R2, R4, L1, L4, R1, R185, R3, L4, L1, L1, L3, R4, L4, L1, R5, L5, L1, R5, L1, R2, L5, L2, R4, R3, L2, R3, R1, L3, L5, L4, R3, L2, L4, L5, L4, R1, L1, R5, L2, R4, R2, R3, L1, L1, L4, L3, R4, L3, L5, R2, L5, L1, L1, R2, R3, L5, L3, L2, L1, L4, R4, R4, L2, R3, R1, L2, R1, L2, L2, R3, R3, L1, R4, L5, L3, R4, R4, R1, L2, L5, L3, R1, R4, L2, R5, R4, R2, L5, L3, R4, R1, L1, R5, L3, R1, R5, L2, R1, L5, L2, R2, L2, L3, R3, R3, R1";
+		assertEquals(252, calcShortestPath(puzzleInput));
+	}
 }
